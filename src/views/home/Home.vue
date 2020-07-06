2590fd7d5dd858fafa6cd814263dcfeda2a8ce85
songyi520 <291520373@qq.com>
2020-07-06 16:13:41 +0800

    1.1.2

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,31 @@
 <template>
 	<div id="home">
-		首页
+		<Header></Header>
+		<Sowing></Sowing>
 	</div>
 </template>
 
 <script>
 	//1 引入
 	import {getHomeData} from './../../service/api/index'
+	//2 引入组件
+	import Header from './components/header/Header'
+	import Sowing from './components/sowing/Sowing'
 	export default{
 		name:"Home",
-		
+		created(){
+			// 2.请求网络数据
+			getHomeData().then((response)=>{
+				console.log(response);
+			}).catch(error=>{
+				// todo...
+				console.log(error);
+			})
+		},
+		components:{
+			Header,
+			Sowing,
+		}
 	}
 </script>
 
---
+++ src/views/home/components/sowing/Sowing.vue
@@ -0,0 +1,25 @@
+<template>
+	 <swiper ref="mySwiper" :options="swiperOptions">
+	    <swiper-slide>Slide 1</swiper-slide>
+	    <swiper-slide>Slide 2</swiper-slide>
+	    <swiper-slide>Slide 3</swiper-slide>
+	    <swiper-slide>Slide 4</swiper-slide>
+	    <swiper-slide>Slide 5</swiper-slide>
+	    <div class="swiper-pagination" slot="pagination"></div>
+	  </swiper>
+</template>
+
+<script>
+	import 'swiper/dist/css/swiper.css'
+	import { swiper, swiperSlide } from 'vue-awesome-swiper'
+	export default{
+		name:"Sowing",
+		components:{
+			swiper,
+			swiperSlide,
+		}
+	}
+</script>
+
+<style>
+</style>
