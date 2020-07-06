0952bbc8de0409ad02cb09b159b84cfb7327a2a2
songyi520 <291520373@qq.com>
2020-07-06 17:54:40 +0800

    1.1.3

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 
+
+
+
 //1 引入 fastclick
 import FastClick from 'fastclick'
 if ('addEventListener' in document) {
@@ -15,6 +18,10 @@ import '@/style/common.less'
 
 //3 引入全局UI组件库-vant
 import '@/plugins/vant'
+//轮播插件
+// import 'swiper/swiper-bundle.css'
+// import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 
 new Vue({
 	router,
--- src/views/home/components/sowing/Sowing.vue
+++ src/views/home/components/sowing/Sowing.vue
@@ -1,19 +1,50 @@
 <template>
-	 <swiper ref="mySwiper" :options="swiperOptions">
-	    <swiper-slide>Slide 1</swiper-slide>
-	    <swiper-slide>Slide 2</swiper-slide>
-	    <swiper-slide>Slide 3</swiper-slide>
-	    <swiper-slide>Slide 4</swiper-slide>
-	    <swiper-slide>Slide 5</swiper-slide>
-	    <div class="swiper-pagination" slot="pagination"></div>
+	 <swiper :options="swiperOption" ref="mySwiper" id="swiper">
+	    <!-- slides -->
+	    <swiper-slide>I'm Slide 1</swiper-slide>
+	    <swiper-slide>I'm Slide 2</swiper-slide>
+	    <swiper-slide>I'm Slide 3</swiper-slide>
+	    <swiper-slide>I'm Slide 4</swiper-slide>
+	    <swiper-slide>I'm Slide 5</swiper-slide>
+	    <swiper-slide>I'm Slide 6</swiper-slide>
+	    <swiper-slide>I'm Slide 7</swiper-slide>
+	    <!-- Optional controls -->
+	    <div class="swiper-pagination"  slot="pagination"></div>
 	  </swiper>
 </template>
 
 <script>
 	import 'swiper/dist/css/swiper.css'
+	
 	import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
 	export default{
 		name:"Sowing",
+		data(){
+			return{
+				swiperOption:{
+					notNextTick:true,
+					//分页
+					pagination:{
+						el:'.swiper-pagination',
+						clickable:true,
+					},
+					//循环
+					loop:true,
+					autoplay:{
+						delay:1000,
+					},
+					//速度
+					speed:600,
+					//滑动后回调函数
+					on:{
+						slideChangeTransitionEnd(){
+							// console.log(this.activeIndex);
+						}
+					}
+				},
+			}
+		},
 		components:{
 			swiper,
 			swiperSlide,
@@ -22,4 +53,9 @@
 </script>
 
 <style>
+	#swiper{
+		width:100%;
+		height:10rem;
+		background-color:red;
+	}
 </style>
